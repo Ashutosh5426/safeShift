0be98db672decff79733211f1e504a963fe1d645
AshutoshAliraHealth <ashutosh.gupta@alirahealth.com>
2025-11-27 17:07:24 +0530

    configured location and map

--- frontend/android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
@@ -1,5 +1,40 @@
 package com.safeShift.app
-
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        createNotificationChannels()
+    }
+
+    private fun createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val geolocatorChannelId = "geolocator_channel"
+            val bgServiceChannelId = "bg_service_channel"
+
+            val geolocatorChannel = NotificationChannel(
+                geolocatorChannelId,
+                "Location Tracking",
+                NotificationManager.IMPORTANCE_LOW
+            ).apply {
+                description = "Notifications for location tracking"
+            }
+
+            val bgServiceChannel = NotificationChannel(
+                bgServiceChannelId,
+                "Background Service",
+                NotificationManager.IMPORTANCE_LOW
+            ).apply {
+                description = "Notifications for background service"
+            }
+
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(geolocatorChannel)
+            manager.createNotificationChannel(bgServiceChannel)
+        }
+    }
+}
--- frontend/ios/Runner/AppDelegate.swift
+++ frontend/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,8 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+//     GMSServices.provideAPIKey("AIzaSyDEsHOhCudunjvxMIXRQi79gVDCIL_5sPA")
+    GMSServices.provideAPIKey("AIzaSyBHhl_m1kcU02O4Ghyg_pRQ9OGk7t0d-Ec")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
